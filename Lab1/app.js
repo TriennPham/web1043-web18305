b5484f49fb4bbd6afc1a1b62fb0f5b235d4ec241
Pham Van Trien <trienpvpd08224@fpt.edu.vn>
2023-03-09 16:09:28 +0700

    up code

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,9 +1,26 @@
+//Nhập vào hai số nguyên a và b từ bàn phím
 let a = prompt("Moi nhap so nguyen a: ");
 let a1 =parseInt(a);
 
 let b = prompt("Moi nhap so nguyen b: ");
 let b1 =parseInt(b);
 
+//Tính tổng c của a và b
 let c= a1 + b1;
 
+//In ra kết quả
 document.write(c);
+
+console.log(c);
+
+//Nhập 1 số nguyên d từ bàn phím
+let d = prompt("Moi nhap so nguyen d: ");
+let d1 = parseInt(d);
+
+//Tính tổng result của c và d
+let result = c + d;
+
+//In ra kết quả
+alert("Tong la " + result);
+
+console.log("Tong la " + result);
